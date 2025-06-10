5c6e0d35df73e027c3a424ed77266a81ad36fdbc
Alex Petrachenka <apetrachenka@decusoft.com>
2025-06-10 09:39:06 +0300

    add Jest setup and testing scripts

--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,9 @@
 module.exports = {
   testEnvironment: 'node',
-  collectCoverageFrom: [
-    'src/**/*.{js,jsx}',
-    '!src/**/*.test.{js,jsx}',
-    '!**/node_modules/**'
-  ],
+  collectCoverageFrom: ['src/**/*.{js,jsx}', '!src/**/*.test.{js,jsx}', '!**/node_modules/**'],
   testMatch: [
     '<rootDir>/src/**/__tests__/**/*.{js,jsx}',
-    '<rootDir>/src/**/*.{test,spec}.{js,jsx}'
+    '<rootDir>/src/**/*.{test,spec}.{js,jsx}',
   ],
-  verbose: true
+  verbose: true,
 };
